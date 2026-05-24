463a85bf36d4c7f69609d78d62a7c9f4fed6d56e
robinsalim100 <robinsalim100@gmail.com>
2026-05-24 21:58:53 +0530

    the change to test

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,4 +11,4 @@ def test_health():
     response = client.get("/health")
 
     assert response.status_code == 200
-    assert response.json() == {"status": "healthy"}
+    assert response.json() == {"status": "we have done it"}
